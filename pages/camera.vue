a30fff75fcb964d42d2fff1c3cfa3a8054baee94
Grisha Dzyin <grishadzyin@gmail.com>
2022-08-03 14:44:26 +0400

    add camera input

--- pages/camera.vue
+++ pages/camera.vue
@@ -1,8 +1,13 @@
 <script setup>
-
+function update(stream) {
+    document.querySelector('video').src = stream.url;
+}
 </script>
 <template>
     <div>
         <h1>camera</h1>
+        <input type="file" id="camera" capture="user" accept="image/*">
+        <!-- <device type="media" onchange="update(this.data)"></device>
+        <video autoplay></video> -->
     </div>
 </template>
--- pages/recipes.vue
+++ pages/recipes.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
         <h1>recipes and search</h1>
+        <NuxtLink to="/camera">
+            <v-btn>СФОТОГРАФИРОВАТЬ!</v-btn>
+        </NuxtLink>
     </div>
 </template>
