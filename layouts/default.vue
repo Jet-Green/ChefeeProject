9ca570ab2d6e21f7c127f726ced94080f0d5a018
Grisha Dzyin <grishadzyin@gmail.com>
2022-08-07 15:03:50 +0400

    search form

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,11 @@
       <h2>Header</h2>
     </v-app-bar>
     <v-main>
-      <slot />
+      <v-row class="d-flex justify-center">
+        <v-col cols="12" md="10" lg="6">
+          <slot />
+        </v-col>
+      </v-row>
     </v-main>
   </v-layout>
 </template>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,7 +2,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, fa } from 'vuetify/iconsets/fa'
-// import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 import { mdi } from "vuetify/lib/iconsets/mdi";
 import '@mdi/font/css/materialdesignicons.css'
 
